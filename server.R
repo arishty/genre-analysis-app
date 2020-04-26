6fca2c431c131bab6d26cc9f4f6528dd33b1b40d
Ali Rishty <tatango7@aol.com>
2020-04-26 18:31:05 -0400

    Added average and summary tabs/plots, set plot colors to be fixed for explore tab

--- server.R
+++ server.R
@@ -9,25 +9,61 @@ function(input, output) {
     
   )
   
+  # glossary_reactive = reactive(
+  # 
+  # )
+  
  
   
-  output$val_eng <- renderPlot(
+  output$explore <- renderPlot(
    songs_genres() %>%  
     ggplot(., aes_string(x=input$x, y=input$y))
-      + geom_point(aes(color = genre), size=6)
+      + geom_point(aes(color = genre), size=6,)
+      + scale_color_discrete(drop = FALSE)   
+   #+ scale_color_gradient(limits=19)
       + ggtitle("Explore Track Metrics by Genre")
       + theme(plot.title = element_text(size=30,lineheight=.8, 
                                      vjust=1)),
     height = 600,
     width = 700
   ) 
-  '
-  output$avg_mode <- renderPlot(
+  
+  output$average <- renderPlot(
     
-     songs_genres() %>% 
-      ggplot(., aes(mean(mode))) + geom_bar()
-      + ggtitle("Average Mode of Selected Genre (0=Minor, 1=Major)")
+     songs_genres() %>% group_by(genre) %>% select(x = input$x, y = input$y, genre) %>%
+        summarize(avg_x=mean(x), avg_y=mean(y)) %>%
+       
+       ggplot(., aes(x=avg_x, y=avg_y))
+     + labs(x = input$x, y = input$y)
+     + geom_point(aes(color = genre), size=6)
+     + ggtitle("Average X/Y by Genre")
+     + theme(plot.title = element_text(size=30,lineheight=.8, 
+                                       vjust=1)),
+     height = 600,
+     width = 700
     
   )
-  '
+  
+  output$summary <- renderPlot(
+    
+    songs %>% group_by(genre) %>% select(genre, y = input$y) %>%
+      summarize(avg_y = mean(y)) %>%
+      
+      ggplot(., aes(x=reorder(genre, avg_y), y= avg_y))
+    + labs(x = "genre", y = input$y)
+    + geom_col(aes(fill=genre))
+    + scale_x_discrete(labels = c("Alternative Rock" = "Alt Rock", "Classic Rock" = "Class. Rock", "Disco/Funk" = "Disco",
+                                  "Progressive Rock" = "Prog Rock", "Video Game Soundtracks" = "VG"))
+    + ggtitle(paste0(input$y, " by genre"))
+    + theme(plot.title = element_text(size=30,lineheight=.8, 
+                                      vjust=1)),
+    height = 600,
+    width = 1000
+    
+  )
+  
+#  output$glossary <-renderText(
+# 
+#  )
+  
   }
--- ui.R
+++ ui.R
@@ -2,44 +2,60 @@
 #everything is comma separated in fluidpage function, all parameters
 
 fluidPage(
+  tags$header(),
   tags$head(tags$style(
     HTML('
-         #sidebar {
+         #title {
             background-color: #1DB954;
+         }
+        #sidebar {
+            background-color: #000000;
+            color: white
+        }
+        #header {
+          padding: 20px;
+          text-align: center;
+          background: #1DB954;
+          color: white;
+          font-size: 20px;
         }
 
-        body, label, input, button, select { 
-          font-family: "Arial";
+        body, label, input, checkboxGroupInput, button, select { 
+          font: "Cambria";
+          font-weight: bold;
         }')
   )),
-  h1("Genretics", id="sidebar"), #shiny::tags$h1 -> tags is writing html for me -> <h1>
+  tags$header(h1("Genretics"), role="banner", id="header"), #shiny::tags$h1 -> tags is writing html for me -> <h1>
   
   sidebarLayout(
     sidebarPanel(
-      
+      id="sidebar",
+      width=3,
       checkboxGroupInput("checked_genres", label = h3("Genres"), 
                          choices = sort(unique(songs$genre)), selected="Alternative Rock"
       ),
       selectizeInput(inputId = "x",
-                     label = "Feature X",
+                     label = "X Metric",
                      choices = names(songs)[4:17],
                      selected = names(songs)[13]
                      ),
       
       selectizeInput(inputId = "y",
-                     label = "Feature Y",
+                     label = "Y Metric",
                      choices = names(songs)[4:17],
                      selected = names(songs)[5]
-
                      )
-      
-      
     ),
+    
+    
     mainPanel(
-      fluidRow(
-        column(12, plotOutput("val_eng"))
+      width=9,
+      tabsetPanel(type = "tabs",
+                  tabPanel("Explore", font="black", plotOutput("explore")),
+                  tabPanel("Average", plotOutput("average")),
+                  tabPanel("Summary of Y Metric", plotOutput("summary"))
+                  #tabPanel("Glossary", ...("glossary"))
       ),
-      #plotOutput("avg_mode")
     )
   )
 
