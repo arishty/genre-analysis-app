a92df076b9cd301cd7fcab323a0bd089d5b73970
Ali Rishty <tatango7@aol.com>
2020-04-26 13:16:39 -0400

    Fixed dynamic axes issue, added app title, sorted genres

--- server.R
+++ server.R
@@ -1,25 +1,30 @@
-library(shiny)
-library(dplyr)
-library(ggplot2)
-library(extrafont)
 
-songs = read.csv(file = 'Spotify_data.csv')
 
 function(input, output) { 
-  output$val_eng <- renderPlot(
+  
+  songs_genres = reactive(
+    
     songs %>%
-      filter(genre == input$checked_genres) %>%
-      ggplot(., aes(x=valence, y=energy))
+      filter(genre == input$checked_genres)
+    
+  )
+  
+ 
+  
+  output$val_eng <- renderPlot(
+   songs_genres() %>%  
+    ggplot(., aes_string(x=input$x, y=input$y))
       + geom_point(aes(color = genre), size=6)
-      + ggtitle("Valence and Energy of Selected Genre")
+      + ggtitle("Explore Track Metrics by Genre")
       + theme(plot.title = element_text(size=30,lineheight=.8, 
-                                      vjust=1)),
-    height = 600, width = 700
+                                     vjust=1)),
+    height = 600,
+    width = 700
   ) 
   '
   output$avg_mode <- renderPlot(
     
-    songs %>% filter(genre==input$check_genres) %>%
+     songs_genres() %>% 
       ggplot(., aes(mean(mode))) + geom_bar()
       + ggtitle("Average Mode of Selected Genre (0=Minor, 1=Major)")
     
--- ui.R
+++ ui.R
@@ -1,29 +1,45 @@
-library(shiny)
-
 # fluidPage controls screen org / resizing for you! also sidebar layout does that
 #everything is comma separated in fluidpage function, all parameters
 
-songs = read.csv(file = 'Spotify_data.csv')
-
-
 fluidPage(
-  
-  #titlePanel(img(src= 'spotify_logo.jpg', width="50%")), #shiny::tags$h1 -> tags is writing html for me -> <h1>
-  
+  tags$head(tags$style(
+    HTML('
+         #sidebar {
+            background-color: #1DB954;
+        }
+
+        body, label, input, button, select { 
+          font-family: "Arial";
+        }')
+  )),
+  h1("Genretics", id="sidebar"), #shiny::tags$h1 -> tags is writing html for me -> <h1>
   
   sidebarLayout(
     sidebarPanel(
       
       checkboxGroupInput("checked_genres", label = h3("Genres"), 
-                         choices = unique(songs$genre), selected="Progressive Rock"
-      )
+                         choices = sort(unique(songs$genre)), selected="Alternative Rock"
+      ),
+      selectizeInput(inputId = "x",
+                     label = "Feature X",
+                     choices = names(songs)[4:17],
+                     selected = names(songs)[13]
+                     ),
+      
+      selectizeInput(inputId = "y",
+                     label = "Feature Y",
+                     choices = names(songs)[4:17],
+                     selected = names(songs)[5]
+
+                     )
+      
       
     ),
     mainPanel(
       fluidRow(
         column(12, plotOutput("val_eng"))
-        #column(6, plotOutput("avg_mode"))
-      )
+      ),
+      #plotOutput("avg_mode")
     )
   )
 
